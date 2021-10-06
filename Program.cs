bcc1876a8766b92be872f2d9f3ca5abc5504babd
polchickk <90201442+polchickk@users.noreply.github.com>
2021-10-06 22:48:13 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Математические_формулы.Методы
+{
+    class Program
+    {
+        static void Main(string[] args)
+  
+        {
+            Console.WriteLine("Введите произвольную переменную");
+            var x = double.Parse(Console.ReadLine());
+            printf(x);
+            
+            Console.ReadKey();
+
+
+        }
+
+        private static double printf(double x)
+        {
+            return 2 * (Math.Sin(Math.Sqrt(x * x + 4) / 2) * Math.Cos(Math.Sqrt(x * x + 1) / 2));
+            Console.ReadKey();
+        }
+
+    }
+
+}
