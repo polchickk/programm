7de99badbf9eb6f7450a91cf34880d93e3c322c9
polina <p.l.pavlova@urfu.me>
2021-10-11 13:11:10 +0500

    culture

--- одиннадцатоеоктября/одиннадцатоеоктября/Program.cs
+++ одиннадцатоеоктября/одиннадцатоеоктября/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace одиннадцатоеоктября
 {
@@ -10,6 +11,27 @@ namespace одиннадцатоеоктября
     {
         static void Main(string[] args)
         {
+            TestCulture(CultureInfo.CurrentCulture.Name);
+            TestCulture("en-US");
+            TestCulture("cz-CZ");
+            Console.ReadKey();
+        }
+        static void TestCulture(string cultureName)
+        {
+            var oldCulture = CultureInfo.CurrentCulture;
+           CultureInfo.CurrentCulture = new CultureInfo(cultureName);
+
+            Console.WriteLine(new string('=', 15));
+
+            Console.WriteLine($"{ CultureInfo.CurrentCulture.Name}:{ CultureInfo.CurrentCulture.DisplayName }");
+            Console.WriteLine(2.5);
+
+            var date = DateTime.Now;
+            Console.WriteLine(date.ToShortDateString());
+            Console.WriteLine(date.ToLongDateString());
+            Console.WriteLine(new string('=', 30));
+            CultureInfo.CurrentCulture = oldCulture;
         }
     }
+
 }
