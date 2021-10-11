677caef184c2484ab6cad8e5fe49bc1109fef1c9
polina <p.l.pavlova@urfu.me>
2021-10-11 15:10:56 +0500

    4задача

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ");
+            var symbol = Console.ReadLine()[0];
+           
+            Console.WriteLine($" Символ \'{symbol}\'");
+            Console.ReadKey();     
+            
+        }
+    }
+}
---
+++ задачи11окт/задачи11окт/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задачи11окт
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //{
+        //    Console.WriteLine(GetHalfString("Hello, world"));
+        //    Console.WriteLine(GetHalfString(""));
+        //    Console.WriteLine(GetHalfString("a b c d"));
+        //    Console.WriteLine("\n Задача 2 \n");
+        //    Console.WriteLine("Введите температуру в градусах по цельсию" );
+        //    var Cel = double.Parse(Console.ReadLine());
+        //    var far = gradvfar(Cel);
+        //    Console.WriteLine($"Температура {Cel:F1} в гр по цельсию в фаренгейте {far:F1}");
+        Console.WriteLine( "\n Задача 3 \n" );
+        Console.WriteLine("Введите адрес электронной почты ");
+        var email = Console.ReadLine();
+        var index = email.IndexOf('@');
+        var mailbox = email.substring(0,index);
+        var mailservice= email.Substring(index + 1);
+        Console.WriteLine($"Ваш почтовый ящик {mailbox} находится на почтовом сервисе {mailservice}");
+            Console.ReadKey();
+        }
+        
+    static string GetHalfString(string fullString)
+        {
+            var s = fullString.Replace(" ", "");
+            return s.Substring(s.Length / 2);
+
+         }
+        static double gradvfar(double cel)
+        {
+
+            return cel * 1.8 + 32;
+        }
+    }
+}
