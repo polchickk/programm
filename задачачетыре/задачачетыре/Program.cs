be7481eef27e9e4e68ad0e4e94b204bc7febb4f9
polina <p.l.pavlova@urfu.me>
2021-10-11 15:27:26 +0500

    задача 4

--- задачачетыре/задачачетыре/Program.cs
+++ задачачетыре/задачачетыре/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace задачачетыре
 {
     class Program
     {
@@ -12,10 +12,10 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Введите символ");
             var symbol = Console.ReadLine()[0];
-           
-            Console.WriteLine($" Символ \'{symbol}\': 0x{Convert.ToString((int)symbol,16).ToUpper()}");
-            Console.ReadKey();     
-            
+
+            Console.WriteLine($" Символ \'{symbol}\': 0x{Convert.ToString((int)symbol, 16).ToUpper()}");
+            Console.WriteLine($"Следующий символ:\'{(char)((int) symbol + 1)}\'");
+            Console.ReadKey();
         }
     }
 }
