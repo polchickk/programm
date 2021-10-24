d265e5eca6823ed8e9ca396ab1042e36578e1f06
polina <p.l.pavlova@urfu.me>
2021-10-24 23:03:48 +0500

    исправленное задание 4

---
+++ task4/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = GetResult();
+            Console.WriteLine("GetResult= {0:F3}", d);
+            Console.ReadKey();
+        }
+        private static double GetResult()
+        {
+            return CalculateExpression(2, 3) + 1/CalculateExpression(5, 7);
+        }
+        static double CalculateExpression(double x, double y)
+        {
+           return (Math.Sin(x) + Math.Sin(y)) / (Math.Cos(x) + Math.Cos(y));
+        }
+    }
+}
