b78bab44a57449c84f0ac4daaa8025d4eb075656
polina <p.l.pavlova@urfu.me>
2021-10-25 13:50:17 +0500

    задания на условные операторы

---
+++ 25october.ifElse/25october/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25october
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите координату x = ");
+            var x = double.Parse(Console.ReadLine());
+            Console.Write("Введите координату y = ");
+            var y = double.Parse(Console.ReadLine());
+
+            if (x == 0)
+                if (y == 0)
+                    Console.Write($"Точка({x};{y}) совпадает с началом координат ");
+                else
+                    Console.Write($"Точка({x};{y}) лежит на оси Oy");
+            else
+                if (y == 0)
+                    Console.Write($"Точка({x};{y}) лежит на оси Ox");
+                else
+                    if (x > 0)
+                        if (y > 0)
+                            Console.WriteLine($"Точка({x};{y}) лежит в I квадранте");
+                        else
+                            Console.WriteLine($"Точка({x};{y}) лежит в IV квадранте");
+                    else
+                        if (y > 0)
+                            Console.WriteLine($"Точка({x};{y}) лежит  вo II квадранте");
+                        else
+                            Console.WriteLine($"Точка({x};{y}) лежит в III квадранте");
+            Console.ReadKey();
+        }
+    }
+}
