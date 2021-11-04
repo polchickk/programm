bf0db27853a696edc114c1e59e1754bccf47a9bb
polina <p.l.pavlova@urfu.me>
2021-11-04 18:55:37 +0500

    выполнено задание 7

---
+++ tasks7/tasks7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение х: ");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Полученное значение фунцкии:" + Function(x));
+            Console.ReadKey();
+        }
+        static double Function(double x)
+        {
+            if (x < -2)
+                return Math.Sqrt(x * x + 4);
+            if (x > 1)
+                return Math.Sqrt(x * x - 1);
+            else
+                return 1 / (x * x + 1);
+
+        }
+    
+    }
+}
