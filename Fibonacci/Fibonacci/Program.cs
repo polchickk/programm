5ebc7ca44b5f53b50496e02146311eb6b428fe40
polina <p.l.pavlova@urfu.me>
2021-11-15 15:36:52 +0500

    числа фибоначчи 15.11.21

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение верхней границы");
+
+            var upperBound = ulong.Parse(Console.ReadLine());
+
+            ulong a = 1, b = 0,c;
+
+            do
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+                Console.Write($"{c}, ");
+
+            } while (c < upperBound);
+
+            Console.ReadKey();
+
+        }
+    }
+}
