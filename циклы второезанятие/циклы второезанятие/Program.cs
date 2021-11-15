9fa85f72a68b686b25f979de8b7dc3c912240a30
polina <p.l.pavlova@urfu.me>
2021-11-15 14:00:41 +0500

    циклы

---
+++ циклы второезанятие/циклы второезанятие/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace циклы_второезанятие
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
